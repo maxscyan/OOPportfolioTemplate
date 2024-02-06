98342e2fad0168daf3ff92a054a2f89f2151410a
Max Siu Chung Yan <143892117+maxscyan@users.noreply.github.com>
2024-02-06 13:31:17 +0000

    Completed week 2 tasks 1 and 2

---
+++ WeeklyExercises/2DataAndExpressions/TaskOne.java
@@ -0,0 +1,17 @@
+public class TaskOne {
+    public static void main (String[] args){
+        System.out.println ("""
+                ///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+                ==          Student Points          ==
+                \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////
+
+
+                Name            Lab     Bonus   Total
+                ----            ---     -----   -----
+                Tom             1       0      1
+                Winston         80      8      88
+                Poppy           57      8      65
+                Sue             40      6      46
+                Charlie         23      2      25""");
+    }
+}
