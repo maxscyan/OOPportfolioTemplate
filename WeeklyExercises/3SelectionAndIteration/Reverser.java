52928d0e8e562c6014353dfef23dd20a4c658532
Max Siu Chung Yan <143892117+maxscyan@users.noreply.github.com>
2024-02-17 11:38:32 +0000

    Completed string reverser program

---
+++ WeeklyExercises/3SelectionAndIteration/Reverser.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Reverser {
+    public static void main(String[] args){
+        String input, reversed = "";
+        char chr;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a string to be reversed: ");
+        input = scan.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            chr = input.charAt(i);
+            reversed = chr + reversed;
+
+        }
+        System.out.println("Word: " + reversed);
+    }
+}
