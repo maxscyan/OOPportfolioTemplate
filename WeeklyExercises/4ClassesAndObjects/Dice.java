fcdd9e7681500ef2d14d30930adc4e8f3f2c50e9
Max Siu Chung Yan <143892117+maxscyan@users.noreply.github.com>
2024-02-19 18:24:47 +0000

    Completed StringPlay.java, started Dice.java

---
+++ WeeklyExercises/4ClassesAndObjects/Dice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Dice {
+    public static void main (String[] args) {
+        String SidesOne, SidesTwo;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How many sides does die 1 have?");
+        SidesOne = scan.next();
+
+        System.out.println("How many sides does die 2 have?");
+        SidesTwo = scan.next();
+
+
+    }
+}
